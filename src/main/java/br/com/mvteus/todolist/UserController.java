90e7060affebffad7e8b21a675bde04c810b937d
Mateus Souza da Silva <mma.teuzzz@gmail.com>
2023-10-10 17:09:45 -0300

    refactor: refactor IUserRepository, UserController and UserModel classes

--- src/main/java/br/com/mvteus/todolist/UserController.java
+++ src/main/java/br/com/mvteus/todolist/UserController.java
@@ -1,5 +1,9 @@
 package br.com.mvteus.todolist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired /* O ciclo de vida da interface será gerenciada pelo Spring */
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+
+            /* Se já existir usuário com esse username
+            * retornar erro e HTTP Status Code correspondente
+            * */
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado com esse username!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/br/com/mvteus/todolist/UserModel.java
+++ src/main/java/br/com/mvteus/todolist/UserModel.java
@@ -1,12 +1,26 @@
 package br.com.mvteus.todolist;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id /* Para definir que este este atributo 'id' seja a chave primária da entidade no banco de dados */
+    @GeneratedValue(generator = "UUID") /* Para que o JPA gere automaticamente as chaves primárias */
+    private UUID id;
     private String name;
     private String username;
     private String password;
+    @CreationTimestamp /* Atributo para ter o registro de quando foi gerado o dado no banco de dados */
+    private LocalDateTime createdAt;
 
     /*public String getName() {
         return name;
